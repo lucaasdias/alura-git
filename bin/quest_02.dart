54740078287920a583ac0f440c5040c99928c0ca
Lucas Dias <lucaasdias@gmail.com>
2022-04-07 10:54:56 -0300

    Atualização de variavel na quest_02

--- bin/quest_02.dart
+++ bin/quest_02.dart
@@ -9,9 +9,9 @@ void main() {
   print("Qual nome do funcionario?");
   String? nome = stdin.readLineSync();
 
-  num salarioBruto = salarioMin * quantSalario;
-  print("O salario bruto do $nome é $salarioBruto");
+  num salBruto = salarioMin * quantSalario;
+  print("O salario bruto do $nome é $salBruto");
 
-  num salarioLiquido = salarioBruto * 0.3;
-  print("O salario liquido do $nome é $salarioLiquido");
+  num salLiquido = salBruto * 0.3;
+  print("O salario liquido do $nome é $salLiquido");
 }
